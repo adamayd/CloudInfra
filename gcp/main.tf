3b1e2bc285179d8430429d01f0db66b80d50cef6
Adam Ayd <adam.ayd@gmail.com>
2021-01-23 01:23:54 -0500

    Recreated ACE bootcamp containers course environment in terraform.

--- gcp/main.tf
+++ gcp/main.tf
@@ -5,7 +5,37 @@ provider "google" {
   zone        = "us-east1-b"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "ace_vpc_network-2"
+resource "google_compute_instance" "vm_instance" {
+  name         = "ace-instance-1"
+  machine_type = "g1-small"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  service_account {
+    scopes = ["storage-ro", "logging-write", "monitoring-write", "pubsub", "service-management", "service-control", "trace"]
+  }
 }
 
+resource "google_compute_network" "vpc_network" {
+  name                    = "ace-vpc-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_firewall" "vpc_firewall" {
+  name    = "allow-all-ingress-firewall-rule"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "all"
+  }
+}
