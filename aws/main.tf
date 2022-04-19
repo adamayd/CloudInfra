36229f32d8e32b48f858a8a606bf35117a2fdda1
Adam Ayd <adam.ayd@gmail.com>
2022-04-18 20:28:52 -0400

    Added basic budget to AWS.

--- aws/main.tf
+++ aws/main.tf
@@ -1,3 +1,16 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_budgets_budget" "testing-only" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "20.00"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2022-04-01_00:01"
+}
+
 variable "http_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
@@ -7,10 +20,6 @@ variable "http_port" {
 output "alb_dns_name" {
   value       = aws_lb.webserver_lb.dns_name
   description = "The domain name of the load balancer"
-}
-
-provider "aws" {
-  region = "us-east-2"
 }
 
 resource "aws_launch_configuration" "webserver_app" {
